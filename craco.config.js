f8fd4d533526357e9e4808efe8d19c7244050516
sheep-hY <1441613931@qq.com>
2022-11-21 18:28:53 +0800

    暴露配置项

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,9 @@
 const path = require('path')
+const pxToViewport = require('postcss-px-to-viewport')
+const vw = pxToViewport({
+    // 视口宽度 一般是375
+    viewportWidth: 375
+})
 module.exports = {
     // webpack 配置
     webpack: {
@@ -7,8 +12,13 @@ module.exports = {
             // 约定：使用@表示SRC 文件所在路径
             "@": path.resolve(__dirname, 'src'),
             // 约定：使用@scss表示样式 文件所在路径
-            "@scss": path.resolve(__dirname, 'src','assets','style'),
-            
+            "@scss": path.resolve(__dirname, 'src', 'assets', 'style'),
+
         }
-    }
+    },
+        style: {
+            postcss: {
+                plugins: [vw]
+            }
+        }
 }
